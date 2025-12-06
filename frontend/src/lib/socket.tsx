7683c7047f36f42c3dc81f0a7aea0ecaccba9537
TanviVVCE <tanvipatil843@gmail.com>
2025-12-05 23:54:14 -0500

    Missing .tsx files added

---
+++ frontend/src/lib/socket.tsx
@@ -0,0 +1,25 @@
+import React, { createContext, useContext, useEffect, useState, ReactNode } from "react";
+import { io, Socket } from "socket.io-client";
+
+//Dummy Comment
+type SocketContextType = { socket: Socket | null };
+const SocketContext = createContext<SocketContextType>({ socket: null });
+
+type SocketProviderProps = { children: ReactNode };
+
+export const SocketProvider = ({ children }: SocketProviderProps) => {
+  const [socket, setSocket] = useState<Socket | null>(null);
+
+  useEffect(() => {
+    const s = io("http://localhost:4000", { transports: ["websocket"] }); //REMOVE IF NEEDED - 0
+    setSocket(s);
+
+    return () => {
+      s.disconnect();
+    };
+  }, []);
+
+  return <SocketContext.Provider value={{ socket }}>{children}</SocketContext.Provider>;
+};
+
+export const useSocket = () => useContext(SocketContext);
